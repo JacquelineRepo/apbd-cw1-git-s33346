e523459ac556fdef14d1dbe2fad375c74a6ac7d1
JacquelineRepo <jakubkarczewski3409@gmail.com>
2026-03-17 11:17:41 +0100

    Added another print to Program.cs, definied variables for Nai + constructor

--- tutorial2/tutorial2/Nai.cs
+++ tutorial2/tutorial2/Nai.cs
@@ -2,5 +2,12 @@
 
 public class Nai
 {
-    
+    private int x;
+    private int y;
+
+    public Nai()
+    {
+        x = 0;
+        y = 0;
+    }
 }
--- tutorial2/tutorial2/Program.cs
+++ tutorial2/tutorial2/Program.cs
@@ -5,6 +5,7 @@ public class Program
     public static void Main()
     {
         Console.Write("Hello World!");
+        Console.Write("ITT JUST WORRKS IT JUST WORKS");
         
         
     }
