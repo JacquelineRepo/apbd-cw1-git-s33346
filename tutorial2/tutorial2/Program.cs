dc3ebf2fa9b1e7c56aef28bb1ad119bf3748406c
JacquelineRepo <jakubkarczewski3409@gmail.com>
2026-03-17 11:29:36 +0100

    Changed console write in feature-conflict

--- tutorial2/tutorial2/Program.cs
+++ tutorial2/tutorial2/Program.cs
@@ -7,7 +7,7 @@ public class Program
         Console.Write("Hello World!");
         Console.Write("ITT JUST WORRKS IT JUST WORKS");
 
-        Console.Write("More words for more commits");
+        Console.Write("Seasons greetings from feature-conflict!");
         int x = 0;
 
 
